861f9e8166d08f6b0e59ca095571f681e260e182
Peter Sammon <petersammon2@gmail.com>
2020-05-20 21:35:26 -0400

    Psuedocode script.js

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -1,80 +1,35 @@
-alert('the quiz is hard');
 // When user first accesses the page, they are shown an intro message
+// a seperate link to the high scores page
 //  -in the html we have a div in the intro
+// 	- set the questions and it's answers to an array
+// 	- timer is set to 75s
+// 	- hide next button
+// when the user clicks start:
+var startButton = document.getElementById('start-btn');
+// when clicked on, run function, startQuiz
+startButton.addEventListener('click', startQuiz);
 
-// define a quizTimer variable that has no value. We're doing this because
-// we want the gameTimer to be scoped globally, but we don't want to assign
-// it a value until the quiz starts
+function startQuiz() {
+	console.log('started');
+	// 	- the starting div gets hidden
+	$('#question-container').hide();
+	// 	- timer starts on first question
+	// 		- timer is getting decrement by one second
+	// 		- deduct x amount of time when question is answered wrong
+	// 		- if correct do nothing
+	// 	- append questions with it's corresponding multiple choices
+	// loop through the array of the questions
+	// when the next button is clicked on the next item in the array is shown
+	// when the entire array is ran through completely, the quiz is over
+	// 	- when quiz is over:
+	// 		- hide questions
+	// 		- a form input to type initials is shown
+	// 		- when initials is typed in, click submit
+	// 		- when submit is clicked on got to highscores page
+	// and the values are set inside of local storage
+	// 		- when 'go back' is clicked go back to the starting div and are able to begin the quiz again
 
-// when the #start-quiz button is clicked:
-//  - call the startQuiz function
-//  - store the quiz timer, set to zero by default
-//  - store the current question that is being answered
-//  - array of possible answers (lists)
-//  - questions (string)
-//  - correct answers (number, index of the answer from the array of possible answers)
-// Hide the #intro div
-// create a setInterval of 75000ms
-// update the current count to 75
-// every time the interval runs, we need to decrement the current count by 1. re-render the current count
-// render the first question
-// by using DOM manipulation, create a new element(document.createElement) with the question info, and display it on the screen(appendChild)
-
-// once the game has started, display the first question, with the buttons for the answers
-
-// when a user clicks on one of those questions, determine if it is correct or incorrect, display the next question, and display the status of the answer (correct/incorrect, for a short period of time)
-
-// end condition 
-//  - user answers all questions
-//      1. function for handling the end of quiz
-//      2. stop the interval, so that the timer does not continue
-//      3. get our results
-//      4. display the results and initials to the user
-//      5. save the results to local storage
-//          - get any current results, so that we can maintain those results
-//          - add the new results to that list if results
-//      6. create object that has the current result, number of questions, number of answers and how long it took the user to take the quiz
-//      7. resave all of the results with the new array we've added to
-
-//  - time runs out
-
-// get current results
-// get the results from local storage, this should be a JSON string of an array
-// there are any results
-//  - parse the JSON string back into the array
-// if there were no results found in local storage, just return an empty array
-
-//  when the game ends, allow the user to enter their initials, and store their score
-// calculate our results based on number of questions correct, and amount of time left
-//  - generate our results
-//  - if the calculated result is less than 0, return the result
-//  - return the minimum result
-//  - otherwise return calculated score
-
-// reset the quiz vars
-// hide the result container
-// start the quiz again
-
-// make sure the results list is empty in case were displaying it another time
-// get all of the results
-// hide the result container
-// create a new table element with an id
-// create a thread element with all of the headings
-// add the tableHead to the table element
-// create a new tbody element that all of out datais going to be added to
-
-//  redirect the user to the high scores page, which will display all of their stored high scores
-// loop over all of the results
-// create a new row element
-// create a td for the results, and set it's text
-// create a td for questionCorrect and set it's text
-// create a td for time, and set it's text
-// append all of our tds to the row
-// append the row to the table body
-// append the table body to the table element
-// put the entire table element on the document
-// display the results list
-
-
-
-
+	// list of initials(from local storage) and their scores are displayed  onto the high scores page
+	// clear high scores button
+	// 	- when clear high scores is clicked on, clear values that were gotten from local storage
+}
