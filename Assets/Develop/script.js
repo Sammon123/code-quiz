7f6c47a599bdc375e918be677e974a05400b766c
Peter Sammon <petersammon2@gmail.com>
2020-05-20 22:46:13 -0400

    function startQuiz

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -37,8 +37,11 @@ var questions = [
 ];
 // 	- timer is set to 75s
 var quizTimer = 0;
+var time = questions.length * 10;
+var indexOfQuestions;
 // 	- hide next button
 // when the user clicks start:
+// var questionsEl = document.getElementById('question');
 var nextButton = document.getElementById('next-btn');
 var startButton = document.getElementById('start-btn');
 // when clicked on, run function, startQuiz
@@ -46,27 +49,33 @@ $('#next-btn').hide();
 startButton.addEventListener('click', startQuiz);
 
 function startQuiz() {
-	console.log('started');
 	// 	- the starting div gets hidden
 	$('#question-container').hide();
-
-	// 	- timer starts on first question
-	// 		- timer is getting decrement by one second
-	// 		- deduct x amount of time when question is answered wrong
-	// 		- if correct do nothing
-	// 	- append questions with it's corresponding multiple choices
-	// loop through the array of the questions
-	// when the next button is clicked on the next item in the array is shown
-	// when the entire array is ran through completely, the quiz is over
-	// 	- when quiz is over:
-	// 		- hide questions
-	// 		- a form input to type initials is shown
-	// 		- when initials is typed in, click submit
-	// 		- when submit is clicked on got to highscores page
-	// and the values are set inside of local storage
-	// 		- when 'go back' is clicked go back to the starting div and are able to begin the quiz again
-
-	// list of initials(from local storage) and their scores are displayed  onto the high scores page
-	// clear high scores button
-	// 	- when clear high scores is clicked on, clear values that were gotten from local storage
+	// 	- starting the current timer
+	timer = setInterval(quizTimerFunction, 1000);
+	//	- appending the starting time
+	timer.textContent = time;
+	console.log(textContent);
+	// 	- run function to begin the quiz
+	beginQuiz();
 }
+
+// 	- timer starts on first question
+// 		- timer is getting decrement by one second
+// 		- deduct x amount of time when question is answered wrong
+// 		- if correct do nothing
+// 	- append questions with it's corresponding multiple choices
+// loop through the array of the questions
+// when the next button is clicked on the next item in the array is shown
+// when the entire array is ran through completely, the quiz is over
+// 	- when quiz is over:
+// 		- hide questions
+// 		- a form input to type initials is shown
+// 		- when initials is typed in, click submit
+// 		- when submit is clicked on got to highscores page
+// and the values are set inside of local storage
+// 		- when 'go back' is clicked go back to the starting div and are able to begin the quiz again
+
+// list of initials(from local storage) and their scores are displayed  onto the high scores page
+// clear high scores button
+// 	- when clear high scores is clicked on, clear values that were gotten from local storage
