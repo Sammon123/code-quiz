fd8df5a084e5830d0b19149890376f4b408751c7
Peter Sammon <petersammon2@gmail.com>
2020-06-06 17:35:59 -0400

    initial commit

---
+++ Assets/Develop/js/script.js
@@ -0,0 +1,21 @@
+// GIVEN I am taking a code quiz:
+// upon loading the document, the user is going to see the instructions to the quiz
+// these instructions are part of the html. we'll wrap all of the instructions
+// in a div , with an id of #intro
+
+
+// WHEN I click the start button: 
+// hide intro div and start button
+// start timer decrementing from 75s
+// the user is shown the first question of the quiz
+
+// WHEN I answer a question
+//
+// determine whether an answer to specific question is correct or incorrect when answer is clicked on
+// if question is correct, timer remains untouched. when the user clicks 'nextButton' is shown the next question
+// if question is incorrect, deduct some of the time from the timer. when user clicks 'nextButton' the user is presented the next question
+
+// WHEN the game is over the user can:
+// 	- view high scores
+// 	- take quiz again
+// THEN I can save my initials and score
