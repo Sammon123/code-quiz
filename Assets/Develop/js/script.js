de7e09be21700e7628295841ebe424375a3838fd
Peter Sammon <petersammon2@gmail.com>
2020-06-06 18:00:32 -0400

    set timer

--- Assets/Develop/js/script.js
+++ Assets/Develop/js/script.js
@@ -1,12 +1,32 @@
+var quizTime = 0;
 // GIVEN I am taking a code quiz:
 // upon loading the document, the user is going to see the instructions to the quiz
 // these instructions are part of the html. we'll wrap all of the instructions
-// in a div , with an id of #intro
+// in a div , with an class of .container
+
 
 
 // WHEN I click the start button: 
-// hide intro div and start button
+function startQuiz() {
+    // hide intro div and start button
+    startTimer();
+    displayQuestions();
+    $(".container").hide();
+}
+$(".start-button").on("click", startQuiz);
 // start timer decrementing from 75s
+function startTimer() {
+    // store the current time in a var called quizTime. the default is 75
+    quizTime = 75;
+    // use setInterval set to 1sec and assign it to a var
+    quizTimer = setInterval(function () {
+        quizTime--;
+        console.log(quizTime);
+
+    }, 1000);
+};
+
+function displayQuestions() { };
 // the user is shown the first question of the quiz
 
 // WHEN I answer a question
